586f9dbd4809f6755ceaf0a06d441b37d141a113
Tài Lê <96537438+TaiLe1101@users.noreply.github.com>
2025-07-30 11:06:16 +0700

    add: utils-js

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export function sayHello(name: string): string {
-  return `Hello, ${name}!`;
-}
-
 /**
  * Formats a number as a string using the user's locale.
  * @param value The number to format.
@@ -9,24 +5,4 @@ export function sayHello(name: string): string {
  */
 export function formatNumber(value: number): string {
   return new Intl.NumberFormat().format(value);
-}
-
-/**
- * Adds two numbers.
- * @param a The first number.
- * @param b The second number.
- * @returns The sum of the two numbers.
- */
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-/**
- * Subtracts two numbers.
- * @param a The first number.
- * @param b The second number.
- * @returns The difference of the two numbers.
- */
-export function subtract(a: number, b: number): number {
-  return a - b;
 }
